b48ad9948b864598edc320837126653cfa4aa0d1
neoreeps <kenny.speer@gmail.com>
2016-11-13 18:22:05 +0000

    fixup options for usergrid and cassandra

--- provision.sh
+++ provision.sh
@@ -17,7 +17,7 @@ dprint "remove existing container images"
 docker rm -f usergrid cassandra elasticsearch portal
 
 dprint "start containers"
-docker run -d --name cassandra -p 9160:9160 -p 9042:9042 --volume ${PWD}/ug-cassandra/data:/var/lib/cassandra neoreeps/ug-cassandra
-docker run -d --name elasticsearch --volume ${PWD}/ug-elasticsearch/data:/data neoreeps/ug-elasticsearch
-docker run -d --name usergrid --env ADMIN_PASS=${ADMIN_PASS} --env ORG_NAME=${ORG_NAME} --env APP_NAME=${APP_NAME} --link elasticsearch --link cassandra -p 8080:8080 -t neoreeps/usergrid
-docker run -d --name portal --env USERGRID_HOST=${USERGRID_HOST} -p 80:80 neoreeps/ug-portal
+docker run -d --name cassandra -p 9160:9160 -e CASSANDRA_START_RPC="true" -e JAVA_OPTS="-Xms128m -Xmx128m" -p 9042:9042 -v ${PWD}/ug-cassandra/data:/var/lib/cassandra neoreeps/ug-cassandra
+docker run -d --name elasticsearch -v ${PWD}/ug-elasticsearch/data:/data -e ES_JAVA_OPTS="-Des.insecure.allow.root=true -Xms128m -Xmx128m" neoreeps/ug-elasticsearch
+docker run -d --name usergrid -e ADMIN_PASS=${ADMIN_PASS} -e ORG_NAME=${ORG_NAME} -e APP_NAME=${APP_NAME} --link elasticsearch --link cassandra -p 8080:8080 -t neoreeps/usergrid
+docker run -d --name portal -e USERGRID_HOST=${USERGRID_HOST} -p 80:80 neoreeps/ug-portal
