a479a04520faf511618325b46d7a94722634101f
Kenny Speer <kspeer@guardanthealth.com>
2017-02-05 21:32:28 -0800

    fix communication between cassandra and usergrid ... system boots now end-end

--- provision.sh
+++ provision.sh
@@ -13,11 +13,25 @@ dprint() {
 dprint "stop running docker containers"
 docker stop cassandra elasticsearch usergrid portal
 
-dprint "remove existing container images"
+dprint "remove previous cassandra database"
+sudo rm -rf ug-cassandra/data
+
+dprint "remove existing containers"
 docker rm -f usergrid cassandra elasticsearch portal
 
-dprint "start containers"
-docker run -d --name cassandra -p 9160:9160 -e CASSANDRA_START_RPC="true" -e JAVA_OPTS="-Xms128m -Xmx128m" -p 9042:9042 -v ${PWD}/ug-cassandra/data:/var/lib/cassandra neoreeps/ug-cassandra
-docker run -d --name elasticsearch -v ${PWD}/ug-elasticsearch/data:/data -e ES_JAVA_OPTS="-Des.insecure.allow.root=true -Xms128m -Xmx128m" neoreeps/ug-elasticsearch
+dprint "start cassandra"
+docker run -d --name cassandra -e CASSANDRA_START_RPC="true" -e JAVA_OPTS="-Xms256m -Xmx512m" -v ${PWD}/ug-cassandra/data:/var/lib/cassandra neoreeps/ug-cassandra
+
+dprint "start elasticsearch"
+docker run -d --name elasticsearch -v ${PWD}/ug-elasticsearch/data:/data -e ES_JAVA_OPTS="-Des.insecure.allow.root=true -Xms256m -Xmx512m" neoreeps/ug-elasticsearch
+
+while [ -z "$(docker logs cassandra |grep 'Listening for thrift clients')" ];
+do
+    echo "--> waiting for cassandra to finish starting"
+    sleep 2
+done
+
+dprint "start usergrid"
 docker run -d --name usergrid -e ADMIN_PASS=${ADMIN_PASS} -e ORG_NAME=${ORG_NAME} -e APP_NAME=${APP_NAME} --link elasticsearch --link cassandra -p 8080:8080 -t neoreeps/usergrid
+dprint "start usergrid-portal"
 docker run -d --name portal -e USERGRID_HOST=${USERGRID_HOST} -p 80:80 neoreeps/ug-portal
